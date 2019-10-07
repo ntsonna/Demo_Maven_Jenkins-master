0eb351035380a43a0cffe08c471e63c66919101c
unknown <ntsonna@gmail.com>
2019-10-07 08:27:35 +0700

    first commit

---
+++ src/main/java/com/mycompany/mavenproject1_test/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.mavenproject1_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Util.printMessage("Hello World!");
+    }
+}
